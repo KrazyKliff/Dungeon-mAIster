a62f28a5413f6bbff3921914ba219182902b2299
kliff funkface <haggarddalmatian@gmail.com>
2025-08-25 04:00:32 -0400

    fix(ci): correct jest preset path for host-app

--- host-app/jest.config.ts
+++ host-app/jest.config.ts
@@ -1,12 +1,12 @@
 /* eslint-disable */
 export default {
   displayName: 'host-app',
-  preset: './jest.preset.js',
+  preset: '../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: './coverage/host-app',
-  testEnvironment: 'jsdom', // <--- THIS IS THE FIX
+  coverageDirectory: '../coverage/host-app',
+  testEnvironment: 'jsdom',
 };
